6f539680ba0539b6cc6bbd6b3c900cd31f686c6c
Nomi444 <noumanbutt252@gmail.com>
2024-04-03 15:49:29 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load', () => {
+    spinnerWrapperEl.style.opacity = '0';
+
+    setTimeout(() => {
+        spinnerWrapperEl.style.display = 'none';
+
+    }, 200);
+})
